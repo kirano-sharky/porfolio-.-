0e92f76a14771df81c25ad2ef90a50135035a21c
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2023-04-19 22:15:46 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -58,7 +58,7 @@ const About = () => {
           </div>
         </a>
         { /*github icon*/ }
-        <a className='cursor-pointer w-[35px] h-[35px] text-gray-300 hover:bg-black rounded-full transition delay-75' href='https://github.com/meA-sonWat'>
+        <a className='cursor-pointer w-[35px] h-[35px] text-gray-300 hover:bg-white hover:text-black rounded-full transition delay-75' href='https://github.com/meA-sonWat'>
           <div className='relative before:text-[20px] before:z-10 before:absolute before:left-1/2 before:-top-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:-translate-y-full before:rounded-lg before:bg-gray-700 before:px-2 before:py-1.5 before:text-white before:invisible before:content-[attr(data-tip)] after:z-10 after:absolute after:left-1/2 after:-top-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-t-gray-700 after:border-l-transparent after:border-b-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible' data-tip="網站代碼的秘密~">
             <FaGithub/>
           </div>
